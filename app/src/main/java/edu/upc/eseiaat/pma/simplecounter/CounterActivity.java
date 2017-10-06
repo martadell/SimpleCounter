241db46afdcff2ffcdce04866040a1dea04c25f1
Marta <marta.adell.noguera@gmail.com>
2017-10-06 22:39:12 +0200

    counter activity: fer que quan s'apreti el botó + es sumi una xifra al número que apareix per pantalla

--- app/src/main/java/edu/upc/eseiaat/pma/simplecounter/CounterActivity.java
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/CounterActivity.java
@@ -2,12 +2,29 @@ package edu.upc.eseiaat.pma.simplecounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class CounterActivity extends AppCompatActivity {
+
+    TextView txt_number;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_counter);
+        txt_number = (TextView) findViewById(R.id.txt_number);}
+
+   public void clicplus (View v) {
+       Button btn = (Button) v;
+       String number = txt_number.getText().toString();
+       int num = Integer.parseInt(number);
+       num = num + 1;
+       number = String.valueOf(num);
+       txt_number.setText(number);
+
     }
-}
+
+    }
+
