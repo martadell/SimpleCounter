0caa9d4ffc3768b64dbb8eb81419eda120d3a3d6
marta.adell.noguera <marta.adell.noguera@gmail.com>
2017-10-06 16:43:14 +0200

    Crear projecte

---
+++ app/src/main/java/edu/upc/eseiaat/pma/simplecounter/CounterActivity.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.simplecounter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CounterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_counter);
+    }
+}
